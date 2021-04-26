8367c5525b04a9fc05d3e5989905df30cd167fd8
LIA MARIE O. ESTRELLA <dev.lmestrella@gmail.com>
2021-04-26 20:49:23 +0800

    Fix mobile menu position

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ const mobileMenuList = document.querySelector(".site__nav-list");
 const toggleMobileMenu = (e) => {
   mobileMenuLink.classList.toggle("site__hamburger--open");
   mobileMenuList.classList.toggle("site__nav-list--show");
+  document.querySelector("body").classList.toggle("site--fixed");
 };
 
 mobileMenuLink.addEventListener("click", toggleMobileMenu);
